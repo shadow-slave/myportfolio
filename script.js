05d038645e09fb6337f3cc314afa7554a2e1e1d7
shadow-slave <ashwinsunil02@gmail.com>
2024-08-02 17:52:36 +0530

    herosection over

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+function togglemenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".ham-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
